fac60113ec92c6018856cf40ae0a3e0636832515
Feliciafeli <feliciatsumbedzo16@gmail.com>
2021-09-05 21:48:08 +0200

    project added

---
+++ Sebenza2/app/src/main/java/com/example/sebenza/ScreenActivity1.kt
@@ -0,0 +1,11 @@
+package com.example.sebenza
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScreenActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screen1)
+    }
+}
